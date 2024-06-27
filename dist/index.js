4672ac83189d43c45044b237f6af8f039a3bee30
dev-Shivam999 <shivamswami982@gmail.com>
2024-06-27 13:28:38 +0530

    static-2

--- dist/index.js
+++ dist/index.js
@@ -37,7 +37,7 @@ exports.client = new client_1.PrismaClient();
 var app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use('/uploads', express_1.default.static(path_1.default.join(__dirname, '/uploads')));
+app.use('/uploads', express_1.default.static(path_1.default.join(__dirname, './uploads')));
 app.use('/user', userRoute_1.routes);
 dotenv.config(); // Load the environment variables
 app.listen(3000, function () {
